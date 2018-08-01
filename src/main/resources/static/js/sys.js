05aa3cffa77be61a9d65d998380a604e9496fd93
stone <15223093495@163.com>
2018-08-01 11:19:05 +0800

    调整目录结构

--- src/main/resources/static/js/sys.js
+++ src/main/resources/static/js/sys.js
@@ -36,11 +36,11 @@ $(function () {
                     pages = data.data.pages;
                     for (i in datas) {
                         tbody.append('<tr>' +
-                            '<td>' + datas[i].tableName + '</td>' +
-                            '<td>' + datas[i].engine + '</td>' +
-                            '<td>' + datas[i].tableComment + '</td>' +
-                            '<td>' + datas[i].createTime + '</td>' +
-                            '<td><a href="sys/generator/' + datas[i].tableName + '" class="btn btn-info">生成代码</a></td>' +
+                            '<td class="col-md-2"> ' + datas[i].tableName + '</td>' +
+                            '<td class="col-md-1">' + datas[i].engine + '</td>' +
+                            '<td class="col-md-6">' + datas[i].tableComment + '</td>' +
+                            '<td class="col-md-2">' + datas[i].createTime + '</td>' +
+                            '<td class="col-md-1"><a href="sys/generator/' + datas[i].tableName + '" class="btn btn-info">生成代码</a></td>' +
                             '</tr>')
                     }
                 }
