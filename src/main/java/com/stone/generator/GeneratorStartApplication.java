ce25c81eeb54017a2fb0b94efb3f07a0783e2f9c
stone <15223093495@163.com>
2018-08-03 17:13:14 +0800

    1.7

---
+++ src/main/java/com/stone/generator/GeneratorStartApplication.java
@@ -0,0 +1,14 @@
+package com.stone.generator;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Created by Stone on 2018/8/3.
+ */
+public class GeneratorStartApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GeneratorStartApplication.class);
+    }
+}
